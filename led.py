73057ba9f68322892bba1b60baa50739bf0c3cd1
Aadilkho <aadilkhoodoruth@gmail.com>
2020-09-08 17:31:26 +0100

    first commit

--- led.py
+++ led.py
@@ -1,6 +1,15 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
+
+GPIO.output(11, GPIO.HIGH)
+
+time.sleep(0.3)
+
+GPIO.output(11, GPIO.LOW)
+
+time.sleep(0.3)
 
 GPIO.output(11, GPIO.HIGH)
