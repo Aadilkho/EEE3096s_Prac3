8128b4842db2fe917075172091cfa9693f0643cf
Aadil Khoodoruth <36310069+Aadilkho@users.noreply.github.com>
2020-10-29 20:57:54 +0400

    Update Prac 5.py

--- Prac 5.py
+++ Prac 5.py
@@ -7,17 +7,17 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
 # create the spi bus
-	spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
-	# create the cs (chip select)
-	cs = digitalio.DigitalInOut(board.D5)
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
 
-	# create the mcp object
-	mcp = MCP.MCP3008(spi, cs)
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
 
-	# create an analog input channel on pin 0
-	chan = AnalogIn(mcp, MCP.P0)
-	
+# create an analog input channel on pin 0
+chan = AnalogIn(mcp, MCP.P0)
+
 def print_temp_thread():
     """
     This function prints the temperature to the screen every five seconds
