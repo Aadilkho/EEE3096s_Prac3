8252e9653d83e9794e5f0f98e2af4970ecdf9533
Aadil Khoodoruth <36310069+Aadilkho@users.noreply.github.com>
2020-10-29 15:30:30 +0400

    Update Prac 5.py

--- Prac 5.py
+++ Prac 5.py
@@ -1,20 +1,20 @@
 import busio
-2 import digitalio
-3 import board
-4 import adafruit_mcp3xxx.mcp3008 as MCP
-5 from adafruit_mcp3xxx.analog_in import AnalogIn
-6
-7 # create the spi bus
-8 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-9
-10 # create the cs (chip select)
-11 cs = digitalio.DigitalInOut(board.D5)
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
 
 # create the mcp object
-14 mcp = MCP.MCP3008(spi, cs)
-15
-16 # create an analog input channel on pin 0
-17 chan = AnalogIn(mcp, MCP.P0)
-18
-19 print(’Raw ADC Value: ’, chan.value)
-20 print(’ADC Voltage: ’ + str(chan.voltage) + ’V’)
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+chan = AnalogIn(mcp, MCP.P0)
+
+print(’Raw ADC Value: ’, chan.value)
+print(’ADC Voltage: ’ + str(chan.voltage) + ’V’)
