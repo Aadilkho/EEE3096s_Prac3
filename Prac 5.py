a17023d32b1dbead4fd843c212aae4099ea89a04
Aadil Khoodoruth <36310069+Aadilkho@users.noreply.github.com>
2020-10-30 11:34:46 +0400

    Update Prac 5.py

--- Prac 5.py
+++ Prac 5.py
@@ -8,8 +8,7 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 import RPi.GPIO as GPIO
 # Setup board mode
 GPIO.setmode(GPIO.BCM)
-btn_increase=23
-GPIO.setup(btn_increase, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -25,13 +24,23 @@ chan = AnalogIn(mcp, MCP.P0)
 print(' ')
 print('Runtime	'+'Temp Reading	'+'temp	')
 x=0
-def btn_increase_pressed():
-    if btn_value !=2:
-        btn_value=btn_value+1
-    else:
-        btn_value=0
-    GPIO.add_event_detect(btn_increase, GPIO.RISING, callback = btn_increase_pressed , bouncetime=200)
+c=0
+def btn_setup():
+    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(23, GPIO.RISING, callback = btn_pressed , bouncetime=200)
 # Setup debouncing and callbacks
+
+def btn_pressed():
+    global c
+    count=0
+    count=count+1
+    if count=1:
+        c = 10
+    if count = 2:
+        c = 5
+    if count = 3:
+        c = 1
+        cout = 0
 
 
 def print_temp_thread():
@@ -39,27 +48,20 @@ def print_temp_thread():
     This function prints the temperature to the screen every five seconds
     """
     global x
-    if GPIO.input(23)==0:
-        thread = threading.Timer(10.0, print_temp_thread)
-        x = x + 10
-    elif GPIO.input(23)==1:
-        thread = threading.Timer(5.0, print_temp_thread)
-        x=x+5
-    else:
-        thread = threading.Timer(1.0, print_temp_thread)
-        x=x+1
+    global c
     thread.daemon = True  # Daemon threads exit when the program does
     thread.start()
     
     Temp = ((chan.voltage - (0.5))/0.01)
     Temp = round(Temp, 2)
+    
+    x = x + c
     print(x, 's	', chan.value, '		', Temp, 'C')
         
     
 
 if __name__ == "__main__":
     print_temp_thread() # call it once to start the thread
-    btn_increase_pressed()
     
     # Tell our program to run indefinitely
     while True:
