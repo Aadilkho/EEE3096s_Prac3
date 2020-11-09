50dd215df2b88eda4059b2f236e3167c855560bf
Aadil Khoodoruth <36310069+Aadilkho@users.noreply.github.com>
2020-11-09 16:27:26 +0400

    Update Prac 5.py

--- Prac 5.py
+++ Prac 5.py
@@ -81,8 +81,8 @@ def print_temp_thread():
 	
 
 if __name__ == "__main__":
-	btn_setup() #call to check the button press
 	start_setup()
+	btn_setup() #call to check the button press
 	print_temp_thread() # call it once to start the thread
 	
 
